23971509b2e948342994b4fe3cccb5a88d7697a8
sonal119801 <102987792+sonal119801@users.noreply.github.com>
2022-05-26 17:44:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+//determing the clicks and calling the method to create sounds.
+var noOfDrums = document.querySelectorAll(".drum").length;
+
+for( var i=0; i<noOfDrums; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerhtml= this.innerHTML;
+        makeSound(buttonInnerhtml);
+        buttonAnimation(buttonInnerhtml);
+       
+    });
+
+}
+
+//determining the keypress to play sound using keybord.
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+// A function use to determine which sound track to play for which key or ckick.
+
+    function makeSound(key){
+        switch (key) {
+            case "w":
+                var tom1= new Audio("sounds/tom-1.mp3")
+                tom1.play();
+                
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play();
+                
+                break;
+            case "s":
+                var tom3= new Audio("sounds/tom-3.mp3")
+                tom3.play();
+                
+                break;
+            case "d":
+                var tom4= new Audio("sounds/tom-4.mp3")
+                tom4.play();
+                
+                break;
+            case "j":
+                var snare= new Audio("sounds/snare.mp3")
+                snare.play();
+                
+                break;
+            case "k":
+                var crash= new Audio("sounds/crash.mp3")
+                crash.play();
+                
+                break;
+            case "l":
+                var kick= new Audio("sounds/kick-bass.mp3")
+                kick.play();
+                
+                break;
+        
+            default:
+                console.log(buttonInnerhtml);
+                break;
+        }
+
+    }
+
+function buttonAnimation(currentkey){
+    var activebutton= document.querySelector("."+currentkey);
+    activebutton.classList.add("pressed");
+    
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    }, 100);
+
+}
+
